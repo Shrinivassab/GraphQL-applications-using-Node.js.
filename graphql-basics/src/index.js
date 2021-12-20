56ceca80e79ca26ed31b53d1f0f29856e474cc55
shrinivassab <shrinivassab@gmail.com>
2021-12-19 21:31:17 -0500

    Added git ignore

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,141 +1,186 @@
-import { GraphQLServer } from "graphql-yoga";
+import { GraphQLServer } from 'graphql-yoga'
 
-// Demo user Data
+// Scalar types - String, Boolean, Int, Float, ID
+
+// Demo user data
 const users = [{
-   id: '1',
-   name: 'Shrini',
-   email: 'shrini@gmail.com',
-   age: 29,
-
+    id: '1',
+    name: 'Andrew',
+    email: 'andrew@example.com',
+    age: 27
 }, {
     id: '2',
-    name: 'Vass',
-    email: 'vass@gmail.com',
-    age: 25
+    name: 'Sarah',
+    email: 'sarah@example.com'
 }, {
     id: '3',
-    name: 'karnan',
-    email: 'karnan@gmail.com',
-    age: 25
-}];
+    name: 'Mike',
+    email: 'mike@example.com'
+}]
 
-// Demo post data
 const posts = [{
-    id: "1",
-    title: "English",
-    body: "English class",
+    id: '10',
+    title: 'GraphQL 101',
+    body: 'This is how to use GraphQL...',
     published: true,
-    author: "1"
-},{
-    id: "2",
-    title: "Tamil",
-    body: "Tamil class",
-    published: true,
-    author: "1"
-},{
-    id: "3",
-    title: "Math",
-    body: "Math class",
+    author: '1'
+}, {
+    id: '11',
+    title: 'GraphQL 201',
+    body: 'This is an advanced GraphQL post...',
     published: false,
-    author: "2"
-}];
+    author: '1'
+}, {
+    id: '12',
+    title: 'Programming Music',
+    body: '',
+    published: false,
+    author: '2'
+}]
 
 const comments = [{
-    id: '1',
-    text: "Nice book"
-},{
-    id: '2',
-    text: "Good book"
-},{
-    id: '3',
-    text: "Nice book for frontend"
-},{
-    id: '4',
-    text: "Nice book for backend"
-}];
-// Type definition (schema)
+    id: '102',
+    text: 'This worked well for me. Thanks!',
+    author: '3',
+    post: '10'
+}, {
+    id: '103',
+    text: 'Glad you enjoyed it.',
+    author: '1',
+    post: '10'
+}, {
+    id: '104',
+    text: 'This did no work.',
+    author: '2',
+    post: '11'
+}, {
+    id: '105',
+    text: 'Nevermind. I got it to work.',
+    author: '1',
+    post: '11'
+}]
+
+// Type definitions (schema)
 const typeDefs = `
     type Query {
         users(query: String): [User!]!
-        me: User!
         posts(query: String): [Post!]!
         comments: [Comment!]!
+        me: User!
+        post: Post!
     }
-    
+
+    type User {
+        id: ID!
+        name: String!
+        email: String!
+        age: Int
+        posts: [Post!]!
+        comments: [Comment!]!
+    }
+
     type Post {
         id: ID!
         title: String!
         body: String!
         published: Boolean!
         author: User!
+        comments: [Comment!]!
     }
-    
-    type User {
-        id: ID!
-        name: String!
-        email: String!
-        age: Int,
-        posts: [Post!]!
-    }
-    
+
     type Comment {
         id: ID!
         text: String!
+        author: User!
+        post: Post!
     }
 `
 
-// Resolver
+// Resolvers
 const resolvers = {
     Query: {
         users(parent, args, ctx, info) {
             if (!args.query) {
-                return users;
+                return users
             }
+
             return users.filter((user) => {
-                return user.name.toLowerCase().includes(args.query.toLowerCase());
+                return user.name.toLowerCase().includes(args.query.toLowerCase())
             })
-        },
-        me() {
-            return {
-                id: '123123123',
-                name: 'Shrini',
-                email: 'shri@gmail.com',
-                age: 34
-            }
         },
         posts(parent, args, ctx, info) {
             if (!args.query) {
-                return posts;
+                return posts
             }
 
             return posts.filter((post) => {
-               return (post.title.includes(args.query) || post.body.includes(args.query));
-            });
+                const isTitleMatch = post.title.toLowerCase().includes(args.query.toLowerCase())
+                const isBodyMatch = post.body.toLowerCase().includes(args.query.toLowerCase())
+                return isTitleMatch || isBodyMatch
+            })
         },
         comments(parent, args, ctx, info) {
-            return comments;
+            return comments
+        },
+        me() {
+            return {
+                id: '123098',
+                name: 'Mike',
+                email: 'mike@example.com'
+            }
+        },
+        post() {
+            return {
+                id: '092',
+                title: 'GraphQL 101',
+                body: '',
+                published: false
+            }
         }
     },
     Post: {
-        author(parents, args, ctx, info) {
+        author(parent, args, ctx, info) {
             return users.find((user) => {
-               return user.id === parents.author
-            });
+                return user.id === parent.author
+            })
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => {
+                return comment.post === parent.id
+            })
+        }
+    },
+    Comment: {
+        author(parent, args, ctx, info) {
+            return users.find((user) => {
+                return user.id === parent.author
+            })
+        },
+        post(parent, args, ctx, info) {
+            return posts.find((post) => {
+                return post.id === parent.post
+            })
         }
     },
     User: {
-        posts(parents, args, ctx, info) {
-
+        posts(parent, args, ctx, info) {
+            return posts.filter((post) => {
+                return post.author === parent.id
+            })
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => {
+                return comment.author === parent.id
+            })
         }
     }
 }
 
-// Declare the server
 const server = new GraphQLServer({
     typeDefs,
     resolvers
 })
 
 server.start(() => {
-    console.log('The server is up');
+    console.log('The server is up!')
 })
